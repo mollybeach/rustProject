14a5868322d5dd6704823360fb7a0f69012f8790
Molly Beach <mollyjbeach@gmail.com>
2022-03-22 13:55:01 -0700

    added positional arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod print; //mod is a keyword that allows us to create a module
 fn main() {
     print::run();
+    //Print to console
     println!("Number: {}", 1);
+    //Basic Formatting
     println!("{} is from {}", "Brad", "Mass");
+    //Positional Arguments
+    print!("{0} is from {1} and {0} likes to {2}", "Brad", "Mass", "code");
 }
