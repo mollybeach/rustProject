d6bdbf693a27e35bb279ef8f4d92fbd780db08bb
Molly Beach <mollyjbeach@gmail.com>
2022-03-22 15:18:39 -0700

    capacity empty replace

--- src/strings.rs
+++ src/strings.rs
@@ -4,7 +4,21 @@ String = Growable, heap-allocated data structure. Use when you need to modify or
 string data
 */
 pub fn run(){
-    let hello = String::from("Hello"); //growable string
+    let mut hello = String::from("Hello"); //growable string
+    //Get length
+    println!("Length: {}", hello.len());
+    //Push Char 
+    hello.push('W'); //push a char onto the string
+    //Push String
+    hello.push_str("orld!"); //push a string onto the string
     println!("{}", hello);
+    //Capacity in bytes
+    println!("Capacity: {}", hello.capacity());
+    //Check if empty
+    println!("Is Empty: {}", hello.is_empty());
+    //Contains
+    println!("Contains 'World': {}", hello.contains("World"));
+    //Replace
+    println!("Replace: {}", hello.replace("World", "There"));
 
 }
