b7489fc228984b8f522f9704c39e01c98bcff9f3
Molly Beach <mollyjbeach@gmail.com>
2022-03-22 15:02:07 -0700

    boolean functions

--- src/types.rs
+++ src/types.rs
@@ -25,6 +25,11 @@ pub fn run(){
     //Find max size
     println!("Max i32: {}", std::i32::MAX);
     println!("Max i64: {}", std::i64::MAX);
+
+    //Boolean
+    let is_active = true;
+    println!("{}", is_active);
+    println!("{:?}", (x, y, z, is_active));
     
 
 }
