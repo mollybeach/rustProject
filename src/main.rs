572b12cdbc409ea7ad826f0e2d328a8b81463d40
Molly Beach <mollyjbeach@gmail.com>
2022-03-22 13:53:40 -0700

    two string literal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-mod print; //mod is a keyword that allows us to create a module print is the name of the module
+mod print; //mod is a keyword that allows us to create a module
 fn main() {
     print::run();
     println!("Number: {}", 1);
+    println!("{} is from {}", "Brad", "Mass");
 }
