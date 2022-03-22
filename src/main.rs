af806377f45d72833a7ec7432f78d56e3e57c2b0
Molly Beach <mollyjbeach@gmail.com>
2022-03-22 13:56:05 -0700

    added placeholder traits

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,6 @@ fn main() {
     print!("{0} is from {1} and {0} likes to {2}", "Brad", "Mass", "code");
     //Named Arguments
     println!("{name} likes to play {activity}", name="John", activity="Baseball");
+    //Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
 }
