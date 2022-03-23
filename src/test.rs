114f0094aea20678e7bd9e1ab6cfa074ca584211
Molly Beach <mollyjbeach@gmail.com>
2022-03-23 13:02:49 -0700

    lesson 32

--- src/test.rs
+++ src/test.rs
@@ -85,4 +85,45 @@ fn main() {
     }
   }
   
+  // Lesson #32
+fn main() {
+    let first_number = 1;
+    let operator = '-';
+    let second_number = 10;
+  
+    println!("{}", output(first_number, operator, second_number, 0));
+  }
+  
+  fn output(first_number: i32, operator: char, second_number: i32, result: i32) -> String {
+    format!(
+      "{} {} {} = {}",
+      first_number, operator, second_number, result
+    )
+  }
+  
+  
+  
+  #[cfg(test)]
+  mod tests {
+    use crate::main;
+    use crate::output;
+  
+    #[test]
+    fn main_returns_empty_tuple() {
+      assert_eq!(main(), ());
+    }
+  
+    #[test]
+    fn output_expects_four_args() {
+      let out = output(-10, '+', 10, 0);
+      assert_eq!(out, String::from("-10 + 10 = 0"));
+    }
+  
+    #[test]
+    fn operate_expects_three_args() {
+      let op = operate('-', -5, 200);
+      assert_eq!(op, ());
+    }
+  }
+  
   
